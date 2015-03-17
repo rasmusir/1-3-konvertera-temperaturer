6530bb42f972ed550b3e85d7f8dddef017f13485
Rasmus Israelsson <israelssonrasmusraz@gmail.com>
2015-03-17 22:25:08 +0100

    add TemperatureConverter

---
+++ 1-3-konvertera-temperaturer/1-3-konvertera-temperaturer/Model/TemperatureConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1_3_konvertera_temperaturer.Model
+{
+    public static class TemperatureConverter
+    {
+        public static int CelsiusToFarenhiet(int c)
+        {
+            return (int)(c*1.8f + 32);
+        }
+
+        public static int FarenheitToCelsius(int f)
+        {
+            return (int)((f - 32)*5/9);
+        }
+    }
+}
