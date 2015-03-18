6f217dbbbfedefba1e32c4ead44cea7f3b668fe2
Rasmus Israelsson <israelssonrasmusraz@gmail.com>
2015-03-18 16:28:33 +0100

    Final

--- 1-3-konvertera-temperaturer/1-3-konvertera-temperaturer/default.aspx.cs
+++ 1-3-konvertera-temperaturer/1-3-konvertera-temperaturer/default.aspx.cs
@@ -16,42 +16,46 @@ namespace _1_3_konvertera_temperaturer
 
         protected void Convert_Click(object sender, EventArgs e)
         {
-            TablePlaceHolder.Visible = true;
-
-            var step = int.Parse(StepTextBox.Text);
-            var start = int.Parse(StartTextBox.Text);
-            var end = int.Parse(EndTextBox.Text);
-
-            var hr = new TableHeaderRow();
-            var hc = new TableHeaderCell();
-            var hf = new TableHeaderCell();
-            hc.Text = "C&deg;";
-            hf.Text = "F&deg;";
-            hr.Cells.Add(hc);
-            hr.Cells.Add(hf);
-            Table.Rows.Add(hr);
-
-
-            for (int t = start; t<=end; t+= step)
+            Validate();
+            if (IsValid)
             {
-                var row = new TableRow();
-                var c = new TableCell();
-                var f = new TableCell();
+                TablePlaceHolder.Visible = true;
 
-                if (CtoF.Checked)
-                {
-                    f.Text = Model.TemperatureConverter.CelsiusToFarenhiet(t).ToString();
-                    c.Text = t.ToString();
-                }
-                else
-                {
-                    f.Text = t.ToString();
-                    c.Text = Model.TemperatureConverter.FarenheitToCelsius(t).ToString();
-                }
+                var step = int.Parse(StepTextBox.Text);
+                var start = int.Parse(StartTextBox.Text);
+                var end = int.Parse(EndTextBox.Text);
 
-                row.Cells.Add(c);
-                row.Cells.Add(f);
-                Table.Rows.Add(row);
+                var hr = new TableHeaderRow();
+                var hc = new TableHeaderCell();
+                var hf = new TableHeaderCell();
+                hc.Text = "C&deg;";
+                hf.Text = "F&deg;";
+                hr.Cells.Add(hc);
+                hr.Cells.Add(hf);
+                Table.Rows.Add(hr);
+
+
+                for (int t = start; t <= end; t += step)
+                {
+                    var row = new TableRow();
+                    var c = new TableCell();
+                    var f = new TableCell();
+
+                    if (CtoF.Checked)
+                    {
+                        f.Text = Model.TemperatureConverter.CelsiusToFarenhiet(t).ToString();
+                        c.Text = t.ToString();
+                    }
+                    else
+                    {
+                        f.Text = t.ToString();
+                        c.Text = Model.TemperatureConverter.FarenheitToCelsius(t).ToString();
+                    }
+
+                    row.Cells.Add(c);
+                    row.Cells.Add(f);
+                    Table.Rows.Add(row);
+                }
             }
         }
     }
